57a081408bcdcface203396d1f00f9ef18c76df1
hmshoberg <145800386+hmshoberg@users.noreply.github.com>
2024-01-22 15:48:54 -0600

    Initial starting points

---
+++ Starter_Code/static/js/logic.js
@@ -0,0 +1,30 @@
+// Creating the map object
+let myMap = L.map("map", {
+    center: [48.1667, -100.1667],
+    zoom: 4
+  });
+  
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+
+// Use this link to get the GeoJSON data.
+let link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+
+
+// Function to style each marker based on magnitude and depth
+function style(feature) {
+    return {
+      radius: feature.properties.mag * 2,  // Adjust the multiplier for better visualization
+      fillColor: getColor(feature.properties.depth),
+      color: '#000',
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8
+    };
+  }
+
+  
